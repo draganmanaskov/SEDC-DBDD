5834ceadfc66aa1e8206e0c902a1f6ecc7890f82
Dragan <dmanaskov@gmail.com>
2022-05-31 16:46:47 +0200

    Fixed Errors

--- DB-Class1-Homework1/SQLQuery1.sql
+++ DB-Class1-Homework1/SQLQuery1.sql
@@ -1,57 +1,56 @@
 USE [SEDCACADEMYDB]
 
-
 CREATE TABLE [Student](
-	[StudentId][int] IDENTITY(1,1) PRIMARY KEY NOT NULL,
+	[ID][int] IDENTITY(1,1) PRIMARY KEY  NOT NULL,
 	[FirstName][nvarchar](100) NOT NULL,
 	[LastName][nvarchar](100) NOT NULL,
 	[DateOfBirth][date] NOT NULL,
-	[EnrolledDate][date] NULL,
-	[NationalIDNumber][int] NULL,
-	[StudentCardNumber][int]NULL,
+	[EnrolledDate][date] NOT NULL,
+	[Gender][nvarchar](100) NOT NULL,
+	[NationalIDNumber][nvarchar](100) NOT NULL,
+	[StudentCardNumber][nvarchar](100) NOT NULL,
 )
 
 CREATE TABLE [Teacher](
-	[TeacherId][int] IDENTITY(1,1) PRIMARY KEY NOT NULL,
+	[ID][int] IDENTITY(1,1) PRIMARY KEY  NOT NULL,
 	[FirstName][nvarchar](100) NOT NULL,
 	[LastName][nvarchar](100) NOT NULL,
 	[DateOfBirth][date] NOT NULL,
 	[AcademicRank][nvarchar](100) NOT NULL,
-	[HireDate][date] NULL,
+	[HireDate][date] NOT NULL,
 )
 
 CREATE TABLE [Course](
-	[CourseId][int] IDENTITY(1,1) PRIMARY KEY NOT NULL,
+	[ID][int] IDENTITY(1,1) PRIMARY KEY NOT NULL,
 	[Name][nvarchar](100) NOT NULL,
-	[Creadit][int] NOT NULL,
-	[AcademicYear][date] NOT NULL,
-	[Semester][nvarchar](100) NOT NULL,
+	[Credit][int] NOT NULL,
+	[AcademicYear][int] NOT NULL,
+	[Semester][int] NOT NULL,
 )
 
 CREATE TABLE [Grade](
-	[GradeId][int] IDENTITY(1,1) PRIMARY KEY NOT NULL,
-	[StudentId][int] FOREIGN KEY REFERENCES Student(StudentId) NOT NULL,
-	[CourseId][int] FOREIGN KEY REFERENCES Course(CourseId) NOT NULL,
-	[TeacherId][int] FOREIGN KEY REFERENCES Teacher(TeacherId) NOT NULL,
-	[Grade][int] NOT NULL CHECK (Grade between 1 and 5),
-	[Comment][nvarchar](300) NULL,
-	[CreatedDate][date] NULL,
+	[ID][int] IDENTITY(1,1) PRIMARY KEY NOT NULL,
+	[StudentID][int],
+	[CourseID][int],
+	[TeacherID][int],
+	[Grade][int] NOT NULL,
+	[Comment][nvarchar](300) NOT NULL,
+	[CreatedDate][date] NOT NULL,
 )
 
 CREATE TABLE [AchievementType](
-	[AchievementTypeId][int] IDENTITY(1,1) PRIMARY KEY NOT NULL,
+	[ID][int] IDENTITY(1,1) PRIMARY KEY  NOT NULL,
 	[Name][nvarchar](100) NOT NULL,
 	[Description][nvarchar](300) NULL,
-	[ParticipationRate][decimal] NULL,
+	[ParticipationRate][int] NOT NULL,
 )
 
 CREATE TABLE [GradeDetails](
-	[GradeDetailsId][int] IDENTITY(1,1) PRIMARY KEY NOT NULL,
-	[GradeId][int] FOREIGN KEY REFERENCES Grade(GradeId) NOT NULL,
-	[AchievementTypeId][int] FOREIGN KEY REFERENCES AchievementType(AchievementTypeId) NOT NULL,
-	[AchievementPoints][int] NULL,
-	[AchievementMaxPoints][int] NULL,
-	[AchievementDate][date] NULL,
+	[ID][int] IDENTITY(1,1) PRIMARY KEY  NOT NULL,
+	[GradeId][int],
+	[AchievementTypeId][int],
+	[AchievementPoints][int] NOT NULL,
+	[AchievementMaxPoints][int] NOT NULL,
+	[AchievementDate][date] NOT NULL,
 )
-
 
